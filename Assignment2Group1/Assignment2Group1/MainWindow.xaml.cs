4710cc36d69d8740de7fcd4104fb13102d0a7202
krishjan05 <krishjan05@gmail.com>
2016-09-16 19:45:59 -0400

    Added code to calulate the total and added proper error message for invalid input.

--- Assignment2Group1/Assignment2Group1/MainWindow.xaml.cs
+++ Assignment2Group1/Assignment2Group1/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace Assignment2Group1
     /// </summary>
     public partial class MainWindow : Window
     {
+        decimal ClassACost = 15;
+        decimal ClassBCost = 12;
+        decimal ClassCCost = 9;
+        decimal GrandTotal = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int ClassASeatCount, ClassBSeatCount ,ClassCSeatCount;
+            decimal ClassATotal, ClassBTotal, ClassCTotal;
+            try
+            {
+                if(int.TryParse(txtClassA.Text, out ClassASeatCount) &&
+                    int.TryParse(txtClassB.Text, out ClassBSeatCount) &&
+                    int.TryParse(txtClassC.Text, out ClassCSeatCount))
+                {
+                    ClassATotal = ClassASeatCount * ClassACost;
+                    ClassBTotal = ClassBSeatCount * ClassBCost;
+                    ClassCTotal = ClassCSeatCount * ClassCCost;
+                    GrandTotal = ClassATotal + ClassBTotal + ClassCTotal;
+
+                    lblClassATotal.Content = ClassATotal.ToString("n2");
+                    lblClassBTotal.Content = ClassBTotal.ToString("n2");
+                    lblClassCTotal.Content = ClassCTotal.ToString("n2");
+                    lblGrandTotal.Content = "Grand Total = $" + GrandTotal.ToString("n2");
+                    MessageBox.Show("Grand Total = " + GrandTotal.ToString("n2"), "Total Amount");
+                }
+                else
+                {
+                    MessageBox.Show("Please enter valid count of seat", "Error");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "Please enter valid number of seats");
+            }
+
         }
     }
 }
