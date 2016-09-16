a1cd100539c5d31274800d972e15f0ff3241b597
krishjan05 <krishjan05@gmail.com>
2016-09-15 20:10:01 -0400

    Added 3 labels to show Class, Number of Seat, Amount for a class

--- Assignment1Group1/Assignment1Group1/MainWindow.xaml.cs
+++ Assignment1Group1/Assignment1Group1/MainWindow.xaml.cs
@@ -1,9 +1,5 @@
-﻿/* Assignment1Group1
- * Created by Krishna Kanhaiya
- * Created on 11 September 2016
- * This Program translates 3 Latin word viz. sinister, dexter and medium to english as Left, Right and Center respectively
- */
-
+﻿/*This Program displays english word for Latin word
+*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Assignment1Group1
+namespace Grp1_assign1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -31,28 +27,22 @@ namespace Assignment1Group1
             InitializeComponent();
         }
 
-        //Function to display the translation of sinister to English when sinister Button is clicked
-        private void btnEnglishLeft_Click(object sender, RoutedEventArgs e)
+        //When this Button is clicked, it shows english word for Sinister
+        private void btn_sinister_Click(object sender, RoutedEventArgs e)
         {
-            lblLatinLeft.Visibility = Visibility.Visible;
-            lblLatinCenter.Visibility = Visibility.Hidden;
-            lblLatinRight.Visibility = Visibility.Hidden;
+            lbl_center.Content = "Left";
         }
 
-        //Function to display the translation of dexter to English when dexter Button is clicked
-        private void btnEnglishRight_Click(object sender, RoutedEventArgs e)
+        //When this Button is clicked, it shows english word for Dexter
+        private void btn_dexter_Click(object sender, RoutedEventArgs e)
         {
-            lblLatinLeft.Visibility = Visibility.Hidden;
-            lblLatinCenter.Visibility = Visibility.Hidden;
-            lblLatinRight.Visibility = Visibility.Visible;
+            lbl_center.Content = "Right";
         }
 
-        //Function to display the translation of medium to English when medium Button is clicked
-        private void btnEnglishCenter_Click(object sender, RoutedEventArgs e)
+        //When this Button is clicked, it shows english word for Medium
+        private void btn_medium_Click(object sender, RoutedEventArgs e)
         {
-            lblLatinLeft.Visibility = Visibility.Hidden;
-            lblLatinCenter.Visibility = Visibility.Visible;
-            lblLatinRight.Visibility = Visibility.Hidden;
+            lbl_center.Content = "Center";
         }
     }
 }
